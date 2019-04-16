6dad6bf80b300e51a7258819f03a030b133997db
ilyastrodubtsev <ilyastrodubtsev@gmail.com>
2019-04-16 06:17:15 -0400

    add custom package and files

---
+++ stage2/05-add-custom-package-with-build/00-run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+cp -rp files/root/* ${ROOTFS_DIR}/
+cp -R files/PiBot1 ${ROOTFS_DIR}/home/pi/
+cp -rp files/python/* ${ROOTFS_DIR}/usr/local/lib/python3.5/dist-packages/
---
+++ stage2/06-copy-custom-files/00-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+cp -R files/PiBot1/Software/pi/lib/ ${ROOTFS_DIR}/
+cp -R files/PiBot1/Software/pi/opt/ ${ROOTFS_DIR}/
