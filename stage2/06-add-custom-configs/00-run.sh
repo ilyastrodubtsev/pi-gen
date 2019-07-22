0757c022c0414995a3512e4da10ff094da8cb843
ilyastrodubtsev <ilyastrodubtsev@gmail.com>
2019-07-22 07:39:14 -0400

    Renamed config avahi. Modified config Janus.cfg.

--- stage2/06-add-custom-configs/00-run.sh
+++ stage2/06-add-custom-configs/00-run.sh
@@ -4,10 +4,12 @@ cp -R files/pi/opt/ ${ROOTFS_DIR}/
 cp files/wifi-connect.service ${ROOTFS_DIR}/lib/systemd/system/
 cp files/robot.service ${ROOTFS_DIR}/lib/systemd/system/
 cp files/check_internet.service ${ROOTFS_DIR}/lib/systemd/system/
+cp files/avahi-pibot.service ${ROOTFS_DIR}/lib/systemd/system/
 sh -c "echo "bcm2835-v4l2" >> ${ROOTFS_DIR}/etc/modules"
 on_chroot <<EOF
 ln -sv /lib/systemd/system/janus.service /etc/systemd/system/multi-user.target.wants/janus.service
 ln -sv /lib/systemd/system/wifi-connect.service /etc/systemd/system/multi-user.target.wants/wifi-connect.service
 ln -sv /lib/systemd/system/robot.service /etc/systemd/system/multi-user.target.wants/robot.service
 ln -sv /lib/systemd/system/check_internet.service /etc/systemd/system/multi-user.target.wants/check_internet.service
+ln -sv /lib/systemd/system/avahi_pibot.service /etc/systemd/system/multi-user.target.wants/avahi-pibot.service
 EOF
