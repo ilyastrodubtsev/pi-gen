b2964357646e57f088f2f6726cae8af051f25838
ilyastrodubtsev <ilyastrodubtsev@gmail.com>
2019-09-02 01:40:32 -0400

    added missing packages and microphone auto tuning

--- stage2/06-add-custom-configs/00-run.sh
+++ stage2/06-add-custom-configs/00-run.sh
@@ -6,6 +6,9 @@ cp files/robot.service ${ROOTFS_DIR}/lib/systemd/system/
 cp files/check_internet.service ${ROOTFS_DIR}/lib/systemd/system/
 cp files/avahi-pibot.service ${ROOTFS_DIR}/lib/systemd/system/
 sh -c "echo "bcm2835-v4l2" >> ${ROOTFS_DIR}/etc/modules"
+sed -i "19a\amixer -c 1 cset name='Input Mux', 'Mic'" ${ROOTFS_DIR}/etc/rc.local
+sed -i "19a\amixer -c 1 sset Mic Capture cap" ${ROOTFS_DIR}/etc/rc.local
+sed -i '$ai2c-dev' ${ROOTFS_DIR}/etc/modules
 on_chroot <<EOF
 ln -sv -f /lib/systemd/system/janus.service /etc/systemd/system/multi-user.target.wants/janus.service
 ln -sv -f /lib/systemd/system/wifi-connect.service /etc/systemd/system/multi-user.target.wants/wifi-connect.service
