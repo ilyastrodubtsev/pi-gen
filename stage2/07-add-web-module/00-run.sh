3536b9f4ca11c24b8222e701eebdc5671a69237f
ilyastrodubtsev <ilyastrodubtsev@gmail.com>
2019-09-13 04:24:01 -0400

    add autostart avahi service for android device

--- stage2/07-add-web-module/00-run.sh
+++ stage2/07-add-web-module/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 cp -R files/web ${ROOTFS_DIR}/home/pi/web
 cp files/web/etc/nginx/sites-available/robot ${ROOTFS_DIR}/etc/nginx/sites-available/robot
-ln -s /home/pi/web/etc/nginx/sites-available/robot ${ROOTFS_DIR}/etc/nginx/sites-enabled/robot
-rm ${ROOTFS_DIR}/etc/nginx/sites-enabled/default
+ln -sv -f /home/pi/web/etc/nginx/sites-available/robot ${ROOTFS_DIR}/etc/nginx/sites-enabled/robot
+rm -f ${ROOTFS_DIR}/etc/nginx/sites-enabled/default || true
--- stage2/08-wifi/01-run.sh
+++ stage2/08-wifi/01-run.sh
@@ -4,4 +4,6 @@ on_chroot <<EOF
 python3 -m pip install pip --upgrade
 python3 -m pip install python-networkmanager
 python3 -m pip install aiohttp
+python3 -m pip install zeroconf
+python3 -m pip install simplejson
 EOF
